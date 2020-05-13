a1a2c3fa37d568c68e29c27700bef8a2c69fd13a
tsungtingdu <tsungtingdu@gmail.com>
2020-05-13 20:29:29 +0800

    crate restaurant model

---
+++ db/migrate/20200513122314_create_restaurants.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :tel
+      t.string :address
+      t.string :opening_hours
+      t.text :description
+      t.timestamps
+    end
+  end
+end
