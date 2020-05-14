b8081d4537a925ee5ec7c8332b8b12ce23879128
tsungtingdu <tsungtingdu@gmail.com>
2020-05-14 18:10:47 +0800

    create seed data for category and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create category seed data
+
+Category.destroy_all
+
+category_list = [
+  { name: '中式料理' },
+  { name: '日本料理' },
+  { name: '義大利料理' },
+  { name: '墨西哥料理' },
+  { name: '素食料理' },
+  { name: '美式料理' },
+  { name: '複合式料理' }
+]
+
+category_list.each do |t|
+  Category.create(name: t['name'])
+end
+
+# create admin
+
+User.create(
+  email: 'root@example.com',
+  password: '12345678',
+  role: 'admin'
+)
+
+puts 'Done!'
