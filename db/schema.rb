95b61404a71058125166d535cd3c725d4bff5919
tsungtingdu <tsungtingdu@gmail.com>
2020-05-14 18:02:35 +0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200513135649) do
+ActiveRecord::Schema.define(version: 20200514100105) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
