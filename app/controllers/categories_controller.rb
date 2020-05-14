364171df7e6589bdfa55066a86445c31232f8eed
tsungtingdu <tsungtingdu@gmail.com>
2020-05-14 21:50:26 +0800

    create show restaurants page with selected category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @restaurants = @category.restaurants.page(params[:page]).per(9)
+  end
+end
