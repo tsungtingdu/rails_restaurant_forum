d15ec71013ef8ee839e29a79780e8b2ba74f6400
tsungtingdu <tsungtingdu@gmail.com>
2020-05-14 21:00:31 +0800

    setup restaurants resources for non-admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'restaurants#index'
+  resources :restaurants, only: %i[index show]
 
   namespace :admin do
     resources :restaurants
