a84aabb9d940ad0275020de8d96399392f3f2c9b
tsungtingdu <tsungtingdu@gmail.com>
2020-05-14 18:28:31 +0800

    add category_id to Restaurant model

---
+++ db/migrate/20200514102707_add_category_id_to_restaurant.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddCategoryIdToRestaurant < ActiveRecord::Migration[5.1]
+  def change
+    add_column :restaurants, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200514100105) do
+ActiveRecord::Schema.define(version: 20200514102707) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20200514100105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
