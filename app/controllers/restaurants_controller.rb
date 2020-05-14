9221429b7cfa7b857ce298bb7c5d27ae50090056
tsungtingdu <tsungtingdu@gmail.com>
2020-05-14 21:04:00 +0800

    create restaurant index page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,9 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @restaurants = Restaurant.page(params[:page]).per(9)
+  end
 
   private
 
