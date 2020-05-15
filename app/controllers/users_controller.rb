dee7f7d4ab00ee24ef786fe7aca4ce81b71a2e4c
tsungtingdu <tsungtingdu@gmail.com>
2020-05-15 11:40:15 +0800

    create user edit and update action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    if @user = current_user
+      @user = User.find(params[:id])
+    else
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :intro)
+  end
 end
