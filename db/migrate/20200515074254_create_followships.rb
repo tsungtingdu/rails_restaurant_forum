9f56d90d9d5fc2a9ab2d9fd31e8d506b2a359da2
tsungtingdu <tsungtingdu@gmail.com>
2020-05-15 15:56:16 +0800

    create followship model and associations

---
+++ db/migrate/20200515074254_create_followships.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateFollowships < ActiveRecord::Migration[5.1]
+  def change
+    create_table :followships do |t|
+      t.integer :user_id
+      t.integer :following_id
+      t.timestamps
+    end
+  end
+end
