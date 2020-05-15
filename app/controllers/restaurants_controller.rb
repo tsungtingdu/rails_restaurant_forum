a287f281ec045ad9860e5e4a4a5c543b0bb9e59b
tsungtingdu <tsungtingdu@gmail.com>
2020-05-15 13:30:39 +0800

    create dashboard action and view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,6 +19,10 @@ class RestaurantsController < ApplicationController
     @recent_comments = Comment.order(created_at: :desc).limit(10)
   end
 
+  def dashboard
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   private
 
   def authenticate_admin
