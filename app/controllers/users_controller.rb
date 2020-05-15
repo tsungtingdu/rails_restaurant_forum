38782f26611b65531cea46d4499d03a24efc7c69
tsungtingdu <tsungtingdu@gmail.com>
2020-05-15 11:53:28 +0800

    create uploading user avatart action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :intro)
+    params.require(:user).permit(:name, :intro, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :comments
+  mount_uploader :avatar, AvatarUploader
 
   def admin?
     role == 'admin'
